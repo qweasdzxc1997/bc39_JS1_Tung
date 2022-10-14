84a67e8ce84906fee75467a993108c43a0913489
qweasdzxc1997 <thanhtung02697@gmail.com>
2022-10-14 17:30:19 +0700

    homework_js1

---
+++ main.js
@@ -0,0 +1,65 @@
+// *bài 1
+// - đầu vào: số lương 1 ngày, số ngày làm
+// - xử lý: lương 1 ngày * số ngày làm
+// - đầu ra : in kết quả tích 2 số
+var luong1ngay = 10000;
+var songaylam = 30;
+var luongthang = luong1ngay * songaylam
+console.log("tien luong : " + luongthang);
+
+
+// *bài 2
+//  - đầu vào: số 1,2,3,4,5
+//  - xử lý : lấy tổng 5 số / 5
+//  - đầu ra : in kết quả
+var num1 = 2
+var num2 = 2
+var num3 = 2
+var num4 = 2
+var num5 = 2
+var trungbinh = (num1 + num2 + num3 + num4 + num5) / 5
+console.log("gia tri trung binh : " + trungbinh);
+
+// *bài 3
+// - đầu vào :
+//   + giá trị usd: 23500
+//   + giá trị vnd --> usd
+// - xử lý: 1 usd quy đổi ra vnd, 23500 * usd
+// - đầu ra : in kết quả
+var giatriusd = 23500
+var usd = 5
+var quydoi = giatriusd * 5
+console.log("gia usd sang vnd:  " + quydoi)
+
+// bài 4 
+//  - đầu vào : 
+//   + chiều dài : 4
+//   + chiều rồng : 6
+//  - xử lý:
+//   + diện tích = dài * rộng
+//   + chu vi = (dài + rộng) * 2
+//  -đầu ra = in kết quả
+var dai = 4
+var rong = 6
+var chuvi = dai * rong
+var dientich = (dai + rong) * 2
+console.log("dien tich:  " + dientich)
+console.log("chu vi : " + chuvi)
+
+// bài 5
+//  - đầu vào:
+//  + ký số 1 : 12
+//  + ký số 2 : 22
+//  - xử lý :
+//    + tính tổng số vừa nhập
+//    + lấy  số hàng đơn vị: int so_hang_dv = so % 10
+//    + lấy  số hàng đơn vị: int so_hang_chuc = so % 10
+var kyso1 = 12
+var kyso2 = 22
+var tongkyso1 = 1 + 2
+var tongkyso2 = 2 + 2
+console.log("tong ky so 1 : " + tongkyso1)
+
+console.log("tong ky so 2 : " + tongkyso2)
+
+
